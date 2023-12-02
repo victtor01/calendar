3c1e3e189fb009c7b2fe55c0a38318458af4edb7
Victor <josevictot.ar@gmail.com>
2023-12-02 02:19:22 -0300

    modify layout login

--- frontend/src/app/(client)/login/page.tsx
+++ frontend/src/app/(client)/login/page.tsx
@@ -100,62 +100,70 @@ export default function Login() {
 
   return (
     <motion.div className="flex w-full h-[100vh]">
-    <S.Poster className="flex flex-col relative w-full h-[100vh] overflow-hidden bg-gradient-45 from-purple-800 to-cyan-500 top-0">
-      <S.Bubble />
-      <header className="z-[20] p-4 text-lg flex justify-between text-white">
-        <button onClick={handleTheme}>
-          <IconTheme />
-        </button>
-      </header>
-      <section className="flex flex-col p-4 ">teste</section>
-    </S.Poster>
-    <Form
-      bgTheme
-      exit={{ opacity: 0 }}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      onSubmit={handleSubmit(onSubmit)}
-      className={`shadow-xl max-w-[27rem] bg-opacity-20 h-auto relative backdrop-blur-3xl p-10 justify-center`}
-    >
-      <div className="flex flex-col mb-10 gap-1">
-        <div
-          className={`text-[2rem] w-full flex justify-center text-cyan-400 ${fontRoboto}`}
-        >
-          Faça o login
+      <S.Poster className="flex flex-col relative w-full h-[100vh] overflow-hidden bg-gradient-45 from-purple-800 to-cyan-500 top-0">
+        <S.Bubble />
+        <header className="z-[20] p-4 text-lg flex justify-between text-white">
+          <button onClick={handleTheme}>
+            <IconTheme />
+          </button>
+        </header>
+        <section className="flex flex-col p-4 px-[10rem] justify-center flex-1">
+          <h1 className={`text-5xl text-white font-sm ${fontOpenSans}`}>
+            A melhor gestão do seu tempo!
+          </h1>
+          <div className="flex">
+            <button></button>
+            <button></button>
+          </div>
+        </section>
+      </S.Poster>
+      <Form
+        bgTheme
+        exit={{ opacity: 0 }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        onSubmit={handleSubmit(onSubmit)}
+        className={`shadow-xl max-w-[27rem] bg-opacity-20 h-auto relative backdrop-blur-3xl p-10 justify-center`}
+      >
+        <div className="flex flex-col mb-10 gap-1">
+          <div
+            className={`text-[2rem] w-full flex justify-center text-cyan-400 ${fontRoboto}`}
+          >
+            Faça o login
+          </div>
+          <div
+            className={`text-[1rem] flex w-full text-center justify-center font-semibold opacity-70`}
+          >
+            Bem vindo de volta! Faça o login digitando seu email logo abaixo.
+          </div>
         </div>
-        <div
-          className={`text-[1rem] flex w-full text-center justify-center font-semibold opacity-70`}
-        >
-          Bem vindo de volta! Faça o login digitando seu email logo abaixo.
-        </div>
-      </div>
 
-      <Input
-        required
-        register={register("email")}
-        className={`border focus:border-cyan-500 rounded `}
-      >
-        <div className="absolute z-10 right-4 pointer-events-none ">
-          <MdMail className="opacity-40" size="20" />
-        </div>
-        <span>Email</span>
-      </Input>
-      {errors.email && <>{errors.email.message}</>}
-      <Input
-        register={register("password")}
-        required
-        type="password"
-        className={`border focus:border-cyan-500 rounded `}
-      >
-        <div className="absolute z-10 right-4 pointer-events-none ">
-          <ImLock className="opacity-40" size="20" />
-        </div>
-        <span>Password</span>
-      </Input>
-      <Button className="bg-gradient-45 from-purple-600 to-cyan-400 w-full py-4 text-white font-normal text-lg rounded font-semibold">
-        Entrar
-      </Button>
-    </Form>
-  </motion.div>
+        <Input
+          required
+          register={register("email")}
+          className={`border focus:border-cyan-500 rounded `}
+        >
+          <div className="absolute z-10 right-4 pointer-events-none ">
+            <MdMail className="opacity-40" size="20" />
+          </div>
+          <span>Email</span>
+        </Input>
+        {errors.email && <>{errors.email.message}</>}
+        <Input
+          register={register("password")}
+          required
+          type="password"
+          className={`border focus:border-cyan-500 rounded `}
+        >
+          <div className="absolute z-10 right-4 pointer-events-none ">
+            <ImLock className="opacity-40" size="20" />
+          </div>
+          <span>Password</span>
+        </Input>
+        <Button className="bg-gradient-45 from-purple-600 to-cyan-400 w-full py-4 text-white font-normal text-lg rounded font-semibold">
+          Entrar
+        </Button>
+      </Form>
+    </motion.div>
   );
 }
